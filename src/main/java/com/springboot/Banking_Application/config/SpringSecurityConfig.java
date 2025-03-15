48ab5b23f7ed3b7dd8e51a6c935d852d7a5dbe70
aayushsaud <saudaayush.dev@gmail.com>
2025-03-15 14:21:33 +0545

    Implemented JWT Authentication

--- src/main/java/com/springboot/Banking_Application/config/SpringSecurityConfig.java
+++ src/main/java/com/springboot/Banking_Application/config/SpringSecurityConfig.java
@@ -1,11 +1,11 @@
 package com.springboot.Banking_Application.config;
 
+import com.springboot.Banking_Application.filter.JwtFilter;
 import com.springboot.Banking_Application.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,12 +14,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
     @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,7 +35,7 @@ public class SpringSecurityConfig {
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/com/springboot/Banking_Application/controller/PublicController.java
+++ src/main/java/com/springboot/Banking_Application/controller/PublicController.java
@@ -2,9 +2,14 @@ package com.springboot.Banking_Application.controller;
 
 import com.springboot.Banking_Application.dto.UserDto;
 import com.springboot.Banking_Application.service.UserService;
+import com.springboot.Banking_Application.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,16 +17,40 @@ import org.springframework.web.bind.annotation.*;
 public class PublicController {
 
     @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
     UserService userService;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    JwtUtil jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck() {
         return "App is live and running.";
     }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<?> createUser (@RequestBody UserDto userDto) {
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signUp (@RequestBody UserDto userDto) {
         UserDto userDto1 = userService.createUser(userDto);
         return new ResponseEntity<>(userDto1, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/log-in")
+    public ResponseEntity<?> logIn (@RequestBody UserDto userDto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userDto.getUserName(), userDto.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userDto.getUserName());
+            String jwtToken = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwtToken, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+        }
     }
 }
