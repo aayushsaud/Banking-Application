eef43f05f4242c874f4c178a8a6dab1572424905
aayushsaud <saudaayush.dev@gmail.com>
2025-03-20 09:43:46 +0545

    Made ninor fixes

--- src/main/java/com/springboot/Banking_Application/dto/UserDto.java
+++ src/main/java/com/springboot/Banking_Application/dto/UserDto.java
@@ -1,15 +1,9 @@
 package com.springboot.Banking_Application.dto;
 
 import com.springboot.Banking_Application.entity.Role;
-import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
 
     private Long id;
@@ -17,4 +11,55 @@ public class UserDto {
     private String password;
     private Role role;
     private List<AccountDto> accounts = new ArrayList<>();
+
+    public UserDto() {
+    }
+
+    public UserDto(Long id, String userName, String password, Role role, List<AccountDto> accounts) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.accounts = accounts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<AccountDto> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<AccountDto> accounts) {
+        this.accounts = accounts;
+    }
 }
--- src/main/java/com/springboot/Banking_Application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/Banking_Application/exception/GlobalExceptionHandler.java
@@ -12,24 +12,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<?> accountNotFoundExceptionHandler(AccountNotFoundException accountNotFoundException){
         ErrorResponse accountNotFound = new ErrorResponse(LocalDateTime.now(), accountNotFoundException.getMessage());
-        return new ResponseEntity<>(accountNotFound, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(accountNotFound);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> userNotFoundExceptionHandler(UserNotFoundException userNotFoundException){
         ErrorResponse userNotFound = new ErrorResponse(LocalDateTime.now(), userNotFoundException.getMessage());
-        return new ResponseEntity<>(userNotFound, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFound);
     }
 
     @ExceptionHandler(InsufficientBalanceException.class)
     public ResponseEntity<?> insufficientBalanceException(InsufficientBalanceException insufficientBalanceException){
         ErrorResponse insufficientBalance = new ErrorResponse(LocalDateTime.now(), insufficientBalanceException.getMessage());
-        return new ResponseEntity<>(insufficientBalance, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(insufficientBalance);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<?> userAlreadyExistsException(UserAlreadyExistsException userAlreadyExistsException){
         ErrorResponse userAlreadyExists = new ErrorResponse(LocalDateTime.now(), userAlreadyExistsException.getMessage());
-        return new ResponseEntity<>(userAlreadyExists, HttpStatus.FORBIDDEN);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(userAlreadyExists);
     }
 }
